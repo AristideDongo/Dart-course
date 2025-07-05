87c391231c234885b56e5b30262b2e0c51ed9c66
AristideDongo <aristidedongo4@gmail.com>
2025-07-05 11:09:51 +0000

    favorite screen

--- lib/main.dart
+++ lib/main.dart
@@ -153,7 +153,7 @@ class _MyHomePageState extends State<MyHomePage> {
         page = GeneratorPage();
         break;
       case 1:
-      page = Placeholder();
+      page = FavoritesPage();
         break;
       default:
         throw UnimplementedError('no widget for $selectedIndex');
@@ -241,36 +241,6 @@ class GeneratorPage extends StatelessWidget {
     );
   }
 }
-
-// class FavoritesPage extends StatelessWidget {
-//   @override
-//   Widget build(BuildContext context) {
-//     var appState = context.watch<MyAppState>();
-
-//     if (appState.favorites.isEmpty) {
-//       return Center(
-//         child: Text('No favorites yet.'),
-//       );
-//     }
-
-//     return ListView(
-//       children: [
-//         Padding(
-//           padding: const EdgeInsets.all(20),
-//           child: Text('You have '
-//               '${appState.favorites.length} favorites:'),
-//         ),
-//         for (var pair in appState.favorites)
-//           ListTile(
-//             leading: Icon(Icons.favorite),
-//             title: Text(pair.asPascalCase),
-//           ),
-//       ],
-//     );
-//   }
-// }
-
-// ...
 class BigCard extends StatelessWidget {
   const BigCard({
     super.key,
@@ -295,5 +265,34 @@ class BigCard extends StatelessWidget {
             style: style,
           ),
         ));
+  }
+}
+
+
+class FavoritesPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    var appState = context.watch<MyAppState>();
+
+    if (appState.favorites.isEmpty) {
+      return Center(
+        child: Text('No favorites yet.'),
+      );
+    }
+
+    return ListView(
+      children: [
+        Padding(
+          padding: const EdgeInsets.all(20),
+          child: Text('You have '
+              '${appState.favorites.length} favorites:'),
+        ),
+        for (var pair in appState.favorites)
+          ListTile(
+            leading: Icon(Icons.favorite),
+            title: Text(pair.asPascalCase),
+          ),
+      ],
+    );
   }
 }
